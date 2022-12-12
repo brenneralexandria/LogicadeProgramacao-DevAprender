31b00b7d96696332443e3b630c088a7d27444d22
brenneralexandria <obrenneralexandria@gmail.com>
2022-12-12 19:32:41 -0300

    Mini Projeto para inverter valores

---
+++ miniProjeto1.js
@@ -0,0 +1,14 @@
+// Objetivo: inverter os valores de a e b.
+
+// Declarando as variáveis.
+let a = 'Vermelho';
+let b = 'Azul';
+
+// invertendo o valor das variáveis.
+let c = a;
+a = b;
+b = c;
+
+// Mostrando os valores no console.
+console.log(a);
+console.log(b);
