1cc92c40afa0f947f46d37c13320ce103ca08da3
brenneralexandria <obrenneralexandria@gmail.com>
2022-12-15 18:51:36 -0300

    Mini Projeto 7

---
+++ miniProjeto7.js
@@ -0,0 +1,15 @@
+// Criar uma função para somar todos os multiplos de 3 e 5
+
+soma(10);
+function soma(limite) {
+    let number3 = 0;
+    let number4 = 0;
+    
+    for(let i = 0; i <= limite; i++) {
+        if(i % 3 === 0)
+            number3 += i;
+        if(i % 5 === 0)
+            number4 += i;    
+    }
+    console.log(number3 + number4);
+}
