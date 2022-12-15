c9fa48eabcc8f438241484ba5758e6c2d669cd25
brenneralexandria <obrenneralexandria@gmail.com>
2022-12-15 10:53:54 -0300

    Mini Projeto 5

---
+++ miniProjeto5.js
@@ -0,0 +1,27 @@
+// Par ou impar 
+
+/* Receber um valor e informa se ele é par ou impar */
+
+
+// Programa para ler qualquer número se é par ou impar
+/* exibirTipo(11);
+
+function exibirTipo(tipo) {
+    if(tipo % 2 === 0)
+        console.log(tipo + ': Par');
+     else {
+        console.log(tipo + ': Impar');
+    }
+}*/
+
+// Ler se é par ou impar em loop
+exibirTipo(11);
+
+function exibirTipo(tipo) {
+    for(let i = 0; i <= tipo; i++)
+        if(i % 2 === 0)
+            console.log(i + ': Par');
+        else {
+            console.log(i + ': Impar');
+        }
+}
