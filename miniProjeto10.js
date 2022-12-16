5d9a488a0cfbc950809eeddd852a141837b0b87c
brenneralexandria <obrenneralexandria@gmail.com>
2022-12-16 09:52:18 -0300

    Mini Projeto 10

---
+++ miniProjeto10.js
@@ -0,0 +1,20 @@
+// Números primos
+
+// Criar uma função para mostrar os números primos
+
+exibirNumeroPrimo(20);
+
+function exibirNumeroPrimo(limite) {
+    for(let numero = 2; numero <= limite; numero++) {
+        if (numeroPrimo(numero)) console.log(numero);
+    }
+}
+
+function numeroPrimo(numero) {
+    for(let divisor = 2; divisor < numero; divisor++) {
+        if(numero % divisor === 0) {
+            return false;
+        }
+    }
+    return true;
+}
