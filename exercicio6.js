fc227158c05321edd1adb0e8f0f08b5697d4349c
brenneralexandria <obrenneralexandria@gmail.com>
2022-12-27 10:27:53 -0300

    Exercicio 6

---
+++ exercicio6.js
@@ -0,0 +1,8 @@
+let idadeDoMano = prompt("Qual é sua idade?");
+idadeDoMano = parseInt(idadeDoMano);
+
+if (idadeDoMano >= 18) {
+    alert('Você é de maior');
+}else {
+    alert('Você é de menor');
+}
